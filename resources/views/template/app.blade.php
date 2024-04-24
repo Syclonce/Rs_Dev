a96b12cedd43418bf52bcb144e70c49d43dde213
jujukonoha <jujukonoha@gmail.com>
2024-04-24 16:39:45 +0700

    menu apotek

--- resources/views/template/app.blade.php
+++ resources/views/template/app.blade.php
@@ -279,6 +279,39 @@
             </ul>
           </li>
           <!-- akhir -->
+          <!-- awal -->
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+          
+                FARMASI
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="{{  route('superadmin.add') }}" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Transaksi</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="./index2.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>DEPO</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="./index3.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Seting</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <!-- akhir -->
           <li class="nav-item">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-tachometer-alt"></i>
