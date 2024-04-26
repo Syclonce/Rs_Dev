7c3043c39b9424c3599b17f9a426c3c9375ae762
Syclonce <Syclonce@gmail.com>
2024-04-27 00:15:48 +0700

    update set Web

--- resources/views/template/app.blade.php
+++ resources/views/template/app.blade.php
@@ -51,7 +51,7 @@
         <form action="{{ route('logout') }}" method="POST" style="border: none; padding: 0; margin: 0;">
             @csrf
             <button type="submit" class="nav-link" style="background: none; border: none;">
-                <i class="fas fa-search"></i>
+                <i class="fa-solid fa-arrow-right-from-bracket"></i>
             </button>
         </form>
       </li>
@@ -75,9 +75,13 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
+    @php
+    // Mengambil data menggunakan model Webset
+    $setweb = App\Models\setweb::first(); // Anda bisa sesuaikan query ini dengan kebutuhan Anda
+    @endphp
     <a href="#" class="brand-link">
-      {{-- <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8"> --}}
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+      <img src="{{ asset('webset/'.$setweb->logo_app) }}" alt="Webset Logo" class="brand-image img-circle elevation-4" style="opacity: .9">
+      <span class="brand-text font-weight-light">{{ $setweb->name_app }}</span>
     </a>
 
     <!-- Sidebar -->
@@ -85,7 +89,7 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          {{-- <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image"> --}}
+          <img src="{{ asset('storage/' . Auth::user()->profile) }}" class="img-circle elevation-2" alt="Profile Photo">
         </div>
         <div class="info">
             @if(Auth::check())
