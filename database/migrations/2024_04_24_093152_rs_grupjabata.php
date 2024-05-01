6ce9b5280db3f1edd67b2fd27a69961a49df54e6
Syclonce <Syclonce@gmail.com>
2024-05-02 02:06:50 +0700

    update data lokasi dan wilayaj di seluruh indonesia

--- database/migrations/2024_04_24_093152_rs_grupjabata.php
+++ database/migrations/2024_04_24_093152_rs_grupjabata.php
@@ -17,8 +17,7 @@ return new class extends Migration
             $table->string('nama_grup_jabatan');
             $table->timestamps(); // Creates 'created_at' and 'updated_at' columns for timestamps
             // // Add foreign key constraint for 'kode_jabatan' field
-            // $table->foreign('kode_jabatan')->references('kode_jabatan')->on('rs_jabatan')
-            //     ->onDelete('restrict')->onUpdate('restrict');
+            $table->foreign('kode_jabatan')->references('kode_jabatan')->on('rs_jabatan')->onDelete('restrict')->onUpdate('restrict');
         });
     }
 
